6be96ef31e3ecf4d7dafa48e05f37dc4fdeaaba2
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-03 23:00:50 +0000

    Added the rightful type annotation for argument max_delay in task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,10 +2,9 @@
 """ASYNC IO Module."""
 import asyncio
 import random
-from typing import Union
 
 
-async def wait_random(max_delay: Union[int, float] = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Wait for a randow second and return the value."""
     wait_time = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
