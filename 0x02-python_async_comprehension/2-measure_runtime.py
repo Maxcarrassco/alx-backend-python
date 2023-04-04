45075c110d46162e8e98e1c4a588c45a590ec172
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 23:04:53 +0000

    Created a function that calculate the runtime of 4 spawn of async_comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""ASYNC Comprehension Module."""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Spawn three async_comprehension and return its runtime."""
+    started = time.perf_counter()
+    await asyncio.gather(
+            async_comprehension(), async_comprehension(),
+            async_comprehension())
+    elased = time.perf_counter() - started
+    return elased
