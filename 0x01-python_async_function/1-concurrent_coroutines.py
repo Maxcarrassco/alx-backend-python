05a56f7f3b31e550757e878f2064a2a326ac264e
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 01:43:19 +0000

    Modified wait_n to return a sorted list of delay time

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,4 +8,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Run wait_random n times and return a list of it output."""
     out = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return out
+    return sorted(out)
