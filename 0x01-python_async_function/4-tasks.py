1ef1c66286fcf8a9cd9000025922714b2862d18c
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 01:14:24 +0000

    Fixed pycodestyle warning in task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,5 +7,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Run task_wait_random n times and return a list of it output."""
-    out = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    out = await asyncio.gather(
+            *(task_wait_random(max_delay) for _ in range(n)))
     return out
