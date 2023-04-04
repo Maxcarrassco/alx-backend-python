29aa82883fc727520fc971a1d2f44b46f2b94e4a
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 22:12:40 +0000

    Created a coroutine that return a generator of float

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""ASYNC Comprehension Module."""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Return a generator that yield a random value between 0 and 10."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
