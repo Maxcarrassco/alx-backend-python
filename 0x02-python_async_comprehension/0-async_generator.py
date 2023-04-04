6a1a966f9d30e89b5466425177055c44ecb1ea49
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 23:34:59 +0000

    Update async_generator to use AsyncIterator as returnType instead of AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ASYNC Comprehension Module."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import AsyncIterator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncIterator[float]:
     """Return a generator that yield a random value between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
