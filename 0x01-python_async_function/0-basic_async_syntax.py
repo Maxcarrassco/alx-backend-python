46790cb9c0e4bfaa68f9fb8737c87f058f5e676d
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 01:33:47 +0000

    Fixed pycodestyle warning in task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,6 +3,7 @@
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a randow second and return the value."""
     wait_time = random.uniform(0, max_delay)
