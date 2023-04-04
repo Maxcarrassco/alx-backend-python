52de2ab57ebbf8d1b9c1e22ddb032a4571634a94
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 01:30:37 +0000

    Fixed bug in task 0 by change time.sleep to asyncio.sleep

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,10 +2,9 @@
 """ASYNC IO Module."""
 import asyncio
 import random
-import time
 
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a randow second and return the value."""
     wait_time = random.uniform(0, max_delay)
-    time.sleep(wait_time)
+    await asyncio.sleep(wait_time)
     return wait_time
