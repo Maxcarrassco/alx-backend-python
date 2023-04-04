749382e6a11d390e737e73dbc449b9673479fc81
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 01:00:55 +0000

    Added the return type of task_wait_random

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ASYNC IO Module."""
-import asyncio
-from typing import List
+from asyncio import Task, create_task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Task:
     """Create and return a task using wait_random."""
-    return asyncio.create_task(wait_random(max_delay))
+    return create_task(wait_random(max_delay))
