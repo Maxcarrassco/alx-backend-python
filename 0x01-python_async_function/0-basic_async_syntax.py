5168cf723c273219c4cd18fa65e143f83e31a789
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-04 00:29:00 +0000

    Added task 1 solution

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,10 +2,10 @@
 """ASYNC IO Module."""
 import asyncio
 import random
-
+import time
 
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a randow second and return the value."""
     wait_time = random.uniform(0, max_delay)
-    await asyncio.sleep(wait_time)
+    time.sleep(wait_time)
     return wait_time
