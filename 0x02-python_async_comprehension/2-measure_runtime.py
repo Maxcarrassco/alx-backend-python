ae16e67ee06878c969ab8e495f9da2b159e22d1b
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-05 00:54:34 +0000

    Reduce duplication by using a loop

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,8 +8,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Spawn three async_comprehension and return its runtime."""
     started = time.perf_counter()
-    await asyncio.gather(
-            async_comprehension(), async_comprehension(),
-            async_comprehension(), async_comprehension())
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     elased = time.perf_counter() - started
     return elased
