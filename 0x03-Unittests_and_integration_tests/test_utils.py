c2d4c3a5e4457cefa968441c2992ed842abbc40e
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-24 05:34:05 +0000

    Created three test cases for the access_nested_map function of the utils script

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Utils Unittest Module."""
+import unittest
+from parameterized import parameterized
+from typing import Sequence, Mapping
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Implement all unittest cases for utils."""
+
+    @parameterized.expand([
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+            ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected):
+        """Test if access nested map return the rightful output."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
